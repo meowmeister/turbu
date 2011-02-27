aa9dc77d37eefd47346b15411d41ee214c70476d
masonwheeler <masonwheeler@9b0120ce-d9de-11dd-87b6-9d1a99559f42>
2011-02-27 02:37:32 +0000

    Added three new editors.

---
+++ turbu/turbu format/design/EB Editors/Characters/EditInventory.pas
@@ -0,0 +1,79 @@
+unit EditInventory;
+
+interface
+
+uses
+   StdCtrls, Classes, Controls, ExtCtrls, DB, DBCtrls, Mask,
+   JvExMask, JvSpin,
+   EventBuilder, EbEdit, dm_database, variable_selector, IDLookupCombo;
+
+type
+   [EditorCategory('Characters', 'Change Inventory', 1)]
+   TfrmEBEditInventory = class(TfrmEbEditBase)
+      grpOperation: TRadioGroup;
+      grpItemCount: TGroupBox;
+      radExactAmount: TRadioButton;
+      radPointer: TRadioButton;
+      spnExactValue: TJvSpinEdit;
+      selValue: TIntSelector;
+      srcItems: TDataSource;
+      cboItemID: TIDLookupCombo;
+      radSpecificItem: TRadioButton;
+      radItemPtr: TRadioButton;
+      selItemID: TIntSelector;
+   protected
+      procedure UploadObject(obj: TEbObject); override;
+      procedure DownloadObject(obj: TEbObject); override;
+      function NewClassType: TEbClass; override;
+      procedure EnableControlsProperly; override;
+   end;
+
+implementation
+uses
+   EB_Characters, EB_Expressions;
+
+{$R *.dfm}
+
+{ TfrmEBEditInventory }
+
+procedure TfrmEBEditInventory.EnableControlsProperly;
+begin
+   EnableControl(cboItemID, radSpecificItem);
+   EnableControl(selItemID, radItemPtr);
+   EnableControl(spnExactValue, radExactAmount);
+   EnableControl(selValue, radPointer);
+   EnableGroupBox(grpItemCount, grpOperation.ItemIndex <> 2);
+end;
+
+function TfrmEBEditInventory.NewClassType: TEbClass;
+begin
+   result := TEBInventory;
+end;
+
+procedure TfrmEBEditInventory.DownloadObject(obj: TEbObject);
+var
+   pair: TIntPair;
+begin
+   obj.Clear;
+   obj.Values.Add(grpOperation.ItemIndex);
+   if grpOperation.ItemIndex <> 2 then
+   begin
+      pair := DownloadValuePtrSelection(radExactAmount, radPointer, spnExactValue, selValue);
+      obj.Values.AddRange(pair);
+   end;
+   obj.Add(DownloadLookupPtrSelection(radSpecificItem, radItemPtr, cboItemID, selItemID, 'Items'));
+end;
+
+procedure TfrmEBEditInventory.UploadObject(obj: TEbObject);
+begin
+   grpOperation.ItemIndex := obj.Values[0];
+   UploadLookupPtrSelection(obj.Components[0] as TEBExpression, radSpecificItem,
+     radItemPtr, cboItemID, selItemID);
+   if grpOperation.ItemIndex <> 2 then
+      UploadValuePtrSelection(obj.Values[1], obj.Values[2], radExactAmount, radPointer,
+        spnExactValue, selValue);
+end;
+
+initialization
+   RegisterEbEditor(TEBInventory, TfrmEBEditInventory);
+end.
