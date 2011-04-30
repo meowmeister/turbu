bb03c0d85fa62eceb5b9c30a8e3d7a5d04e5ccc6
masonwheeler <masonwheeler@9b0120ce-d9de-11dd-87b6-9d1a99559f42>
2011-04-30 12:52:02 +0000

    Splitting the variable selector off into a custom button edit class that other things can derive from.

---
+++ components/TURBU/button_edit.pas
@@ -0,0 +1,70 @@
+{*****************************************************************************
+* The contents of this file are used with permission, subject to
+* the Mozilla Public License Version 1.1 (the "License"); you may
+* not use this file except in compliance with the License. You may
+* obtain a copy of the License at
+* http://www.mozilla.org/MPL/MPL-1.1.html
+*
+* Software distributed under the License is distributed on an
+* "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+* implied. See the License for the specific language governing
+* rights and limitations under the License.
+*
+*****************************************************************************
+*
+* This file was created by Mason Wheeler.  He can be reached for support at
+* www.turbu-rpg.com.
+*****************************************************************************}
+
+unit button_edit;
+
+interface
+uses
+   Messages, Classes, Controls, StdCtrls;
+
+type
+   TRpgCustomButtonEdit = class(TCustomEdit)
+   private
+      FButton: TButton;
+      procedure CMEnabledChanged(var Message: TMessage); message CM_ENABLEDCHANGED;
+   protected
+      procedure ButtonClick(Sender: TObject); virtual; abstract;
+      procedure SetParent(AParent: TWinControl); override;
+   public
+      constructor Create(AOwner: TComponent); override;
+   published
+      property Enabled;
+   end;
+
+implementation
+
+{ TRpgCustomButtonEdit }
+
+procedure TRpgCustomButtonEdit.CMEnabledChanged(var Message: TMessage);
+begin
+   inherited;
+   FButton.Enabled := self.Enabled;
+end;
+
+constructor TRpgCustomButtonEdit.Create(AOwner: TComponent);
+begin
+   inherited Create(AOwner);
+   self.ReadOnly := true;
+end;
+
+procedure TRpgCustomButtonEdit.SetParent(AParent: TWinControl);
+begin
+   inherited SetParent(AParent);
+   if not assigned(FButton) then
+   begin
+      FButton := TButton.Create(self);
+      FButton.Parent := self;
+      FButton.Width := 22;
+      FButton.Height := 22;
+      FButton.Align := alRight;
+      FButton.Caption := '...';
+      FButton.OnClick := self.ButtonClick;
+   end;
+end;
+
+end.
